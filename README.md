611c4f27646d95043b9e3b316a0017f4a7d7f7c8
heypran <pranjal.baweja@gmail.com>
2023-08-27 23:53:46 +0200

    feat: init setup compiler

---
+++ README.md
@@ -0,0 +1,33 @@
+## ZK-Block - EVM Visualizer (WIP)
+
+Website- [zkblock](https://zkblock.app)
+
+![](https://i.imgur.com/waxVImv.png)
+
+This repository is part of zkblock. If you want to add more tools please create a pr with tools details.
+
+#### Run locally
+
+```
+yarn install
+
+```
+
+Then, you can run locally in development mode with live reload:
+
+```
+yarn dev
+```
+
+Open http://localhost:3000 in the browser.
+
+![](https://i.imgur.com/waxVImv.png)
+
+### Contributions
+
+> Everyone is welcome to contribute to this project.
+
+- Add more zk tools or web3 tools including testing libraries, new zk paradigms
+- Improve the site's codebase
+- Discuss ideas in issues
+- Improve documentation
---
+++ src/sol/compiler.ts
@@ -0,0 +1,55 @@
+interface AbiIO {
+  indexed?: boolean;
+  internalType: string;
+  name: string;
+  type: string;
+}
+
+interface Abi {
+  input: AbiIO[];
+  output: AbiIO[];
+  name: string;
+  stateMutability: string;
+  type: string;
+  anonymous?: boolean;
+}
+
+interface ContractData {
+  contractName: string;
+  byteCode: string;
+  abi: Abi[];
+  storageLayout: any;
+  evm: any;
+}
+
+export const compile = (contractCode: string): Promise<ContractData[]> => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(new URL("./solc.worker.ts", import.meta.url), {
+      type: "module",
+    });
+    worker.onmessage = function (e: any) {
+      const output = e.data.output;
+      const result = [];
+      if (!output.contracts) {
+        reject("Invalid source code");
+        return;
+      }
+      for (const contractName in output.contracts["contract"]) {
+        const contract = output.contracts["contract"][contractName];
+        console.log("contract", contract);
+        result.push({
+          contractName: contractName,
+          byteCode: contract.evm.bytecode.object,
+          abi: contract.abi,
+          storageLayout: contract.storageLayout,
+          evm: contract.evm,
+        } as ContractData);
+      }
+      resolve(result);
+    };
+    worker.onerror = reject;
+    worker.postMessage({
+      contractCode: contractCode,
+    });
+  });
+};
